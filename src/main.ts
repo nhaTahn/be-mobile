5c98f69e0fc0f36fdee2560705251420191ced53
Lam Nhat Tan <tan.lamnhat@elca.vn>
2023-12-08 20:10:37 +0700

    change server to https

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,7 @@ async function bootstrap() {
     .setDescription('Your API description')
     .setVersion('1.0')
     .addServer('http://localhost:3000/', 'Local environment')
-    .addServer('https://staging.yourapi.com/', 'Staging')
-    .addServer('https://production.yourapi.com/', 'Production')
+    .addServer('https://let-me-cook.onrender.com/', 'Production')
     .addTag('Your API Tag')
     .build();
 
