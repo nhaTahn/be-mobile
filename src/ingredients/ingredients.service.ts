ec8378db7d829849cdf1dd8e694a0275ec3288b2
Tus <pchungtu@gmail.com>
2024-01-02 18:52:27 +0700

    add delete method ingre

--- src/ingredients/ingredients.service.ts
+++ src/ingredients/ingredients.service.ts
@@ -5,28 +5,39 @@ import { IngredientsDto } from './dto/ingredients.dto';
 
 @Injectable()
 export class IngredientsService {
-    constructor(
-        @Inject('INGREDIENTS_MODEL')
-        private ingredientsModel: Model<Ingredients>,
-    ) {}
-    async create(ingredientsDto: IngredientsDto): Promise<Ingredients> {
-        const ingredients = new this.ingredientsModel(ingredientsDto);
-        return ingredients.save();
-      }
-    
-    async findAll(): Promise<Ingredients[]> {
-    return this.ingredientsModel.find().exec();
+  constructor(
+      @Inject('INGREDIENTS_MODEL')
+      private ingredientsModel: Model<Ingredients>,
+  ) {}
+  async create(ingredientsDto: IngredientsDto): Promise<Ingredients> {
+      const ingredients = new this.ingredientsModel(ingredientsDto);
+      return ingredients.save();
     }
+  
+  async findAll(): Promise<Ingredients[]> {
+  return this.ingredientsModel.find().exec();
+  }
 
-    async findById(id: string): Promise<Ingredients> {
-        try {
-          const ingredients = await this.ingredientsModel.findById(id).exec();
-          if (!ingredients) {
-            throw new NotFoundException(`Recipe with ID ${id} not found`);
-          }
-          return ingredients;
-        } catch (error) {
-          throw new NotFoundException(`Recipe with ID ${id} not found`);
-        }
+  async findById(id: string): Promise<Ingredients> {
+    try {
+      const ingredients = await this.ingredientsModel.findById(id).exec();
+      if (!ingredients) {
+        throw new NotFoundException(`Recipe with ID ${id} not found`);
       }
+      return ingredients;
+    } catch (error) {
+      throw new NotFoundException(`Recipe with ID ${id} not found`);
+    }
+  }
+  async deleteById(id: string): Promise<void> {
+    try {
+      const result = await this.ingredientsModel.findByIdAndDelete(id).exec();
+      if(!result) {
+        throw new NotFoundException(`Recipe with ID ${id} not found`);
+      }    
+    }
+    catch {
+      throw new NotFoundException(`Recipe with ID ${id} not found`);
+    }
+  }
 }
